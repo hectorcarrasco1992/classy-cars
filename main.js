07381767109e4f080f1ea2c7d8752dbac646be87
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-12-10 15:25:51 -0500

    finished

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 class Vehicle {
+  constructor(name,wheelCount,maxSpeed){
+    this.name = name
+    this.wheelCount = wheelCount
+    this.maxSpeed = maxSpeed
+    this.speed = 0
+    
+  }
+
+  accelerate (){
+    if(this.speed<this.maxSpeed){
+      this.speed += (this.maxSpeed/3)
+    }
+  }
 }
 
-class Car {
+class Car extends Vehicle{
+  constructor(name){
+    super(name,4,130)
+    this.name = name
+    
+  }
+  drive(){
+    return 'Vroom. '
+  }
+  
 }
 
-class Ferrari {
+class Ferrari extends Car {
+  constructor(name){
+    super(name,4,180)
+    this.name = 'Ferrari'
+    this.maxSpeed = 180
+  }
+
+  drive(){
+    return 'Vroom. I\'m a Ferrari!'
+  }
+
+  accelerate(){
+    if(this.speed<this.maxSpeed){
+      this.speed += (this.maxSpeed/2)
+    }
+
+  }
 }
 
 
